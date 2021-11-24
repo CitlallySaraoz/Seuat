4e04877d17e123ea34459848a20ff0ede2672480
citlallysaraoz <delacruzsaraozkarlacitlally@gmail>
2021-11-24 10:50:43 -0600

    Creacion de nuevas paginas

---
+++ view/especialidades/index.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <title>Especialidades</title>
+</head>
+
+<body>
+
+    <?php 
+        $paginaweb = 'index_carpetas';
+        
+        include '../seleccion_menu/seleccion_menu.php'; 
+
+        include '../mainHeader/header.php';
+    ?>
+
+
+
+
+    <h1>Este es son los Especialidades</h1>
+
+
+
+    
+    <br>
+    <?php include '../mainFooter/footer.php'; ?>
+    
+</body>
+</html>
